d9b3cff845426a2218112b298d7fcb670c2ee259
FloryanL <floryan.lollivier@gmail.com>
2016-10-04 17:06:25 +0200

    add col-md & margin/padding/color

--- contact.php
+++ contact.php
@@ -12,73 +12,84 @@
     <!-- Place favicon.ico in the root directory -->
 
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-        crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="fonts/font-awesome.css">
     <script src="js/vendor/modernizr-2.8.3.min.js"></script>
 </head>
 
-<body>
+<body class="container-fluid">
+
     <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
     <!--NAVBAR DU SITE-->
-    <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">BOWLING RENNES</a>
-            </div>
-            <div class="collapse navbar-collapse" id="myNavbar">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">accueil</a></li>
-                    <li><a href="#">votre évenement</a></li>
-                    <li><a href="#">News</a></li>
-                    <li><a href="#">infos + contact</a></li>
-                    <li><a href="#">Tarifs</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i>
-                    </a></li>
-                    <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i>
-                    </a></li>
-                    <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i>
-                    </a></li>
-                </ul>
-            </div>
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
         </div>
+        <a class="navbar-brand" href="#">BOWLING RENNES</a>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="navbar-collapse-1">
+
+            <ul class="nav navbar-nav navbar-left">
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Vos évent'</a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Tarifs</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                <li><a href="#">infos & contact</a></li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
     </nav>
 
     <!--Contact -->
-    <section class="plan">
-        <div>
-            <h3>Contact</h3>
+    <section class="col-md-12 plan">
+        <div class="col-md-6">
+            <h3 class="titre_contact">Contact</h3>
+            <div class="block_contact">
+                <p>Pour des informations, des suggestions ou des commentaires, laissez nous votre message à l'adresse suivante:
+                <p><a href="#" class="mail_contact">bowlingrennes@free.fr</a></p>
+                <p>Tel.: 02.99.50.65.53</p>
+            <iframe scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.fr/maps?f=q&amp;source=s_q&amp;hl=fr&amp;geocode=&amp;q=bowling+rennes&amp;sll=46.75984,1.738281&amp;sspn=9.378602,23.203125&amp;ie=UTF8&amp;hq=bowling&amp;hnear=Rennes,+Ille-et-Vilaine,+Bretagne&amp;ll=48.082091,-1.681323&amp;spn=0.065274,0.006295&amp;output=embed" width="450" height="350" frameborder="0"></iframe>
+            </div>
         </div>
-    </section>
 
-    <section class="infos">
-        <div>
+        <div class="col-md-6">
+            <h3 class="titre_plan_acces">Moyen de locomotion</h3>
+            <p>En voiture : par la rocade de Rennes
+            (sortie 6a ou 6b, à l’intersection avec la route de Nantes - N137).</p>
             
+            <p>Depuis le centre-ville : par l’avenue Henri-Fréville.</p>
+            
+            <p>En bus : Ligne 3 (Avenue des Gayeulles - Centre Alma),
+            Ligne 33 (Beaulieu Restau. U. - Piscine Bréquigny).</p>
+            
+            <p>En métro : Direction la Poterie,
+            arrêt Henri Fréville</p>
         </div>
     </section>
 
-    <!--Script à garder à la fin du HTML-->
-    <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-    <script>
-        window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')
-    </script>
-    <script src="js/plugins.js"></script>
-    <script src="js/main.js"></script>
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-        crossorigin="anonymous"></script> 
+    <section class="col-md-12 infos">
+        <div>
+            <h3 class="titre_contact">Infos</h3>
+        </div>
+    </section>
+
 
 
 
